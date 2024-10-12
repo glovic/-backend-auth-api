82157bd31ed75939b7224c02fc973d5d7ac2fa61
glovic <xtineviktor@gmail.com>
2024-10-12 21:17:27 +0100

    feat: add user login functionality with password verification and JWT token generation

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,4 +24,18 @@ exports.register = async (req, res) => {
     }
 };
 
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(401).json({ message: 'Invalid email or password' });
+    }
+};
